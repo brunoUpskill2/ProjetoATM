54ed5d37de89eed2b5106d0b17cf8540d4792f85
rui-silva91 <rui_silva_91@hotmail.com>
2023-11-08 15:58:41 +0000

    urls transactions

--- ATM/Transactions/urls.py
+++ ATM/Transactions/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('balance_inquiry', views.balance_inquiry, name='balance_inquiry'),
+    path('bill_payment', views.bill_payment, name='bill_payment'),
+    path('change_pin', views.change_pin, name='change_pin'),
+    path('deposit', views.deposit, name='deposit'),
+    path('success_page', views.success_page, name='success_page'),
+    path('transaction_history', views.transaction_history, name='transaction_history'),
+    path('transfer', views.transfer, name='transfer'),
+    path('withdrawal', views.withdrawal, name='withdrawal'),
 ]
