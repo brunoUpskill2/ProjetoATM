f17e4b43ff3dccfede27cf4535947dac1c3ddd60
CarlosF80 <133024102+CarlosF80@users.noreply.github.com>
2023-11-09 15:47:33 +0000

    USER ,Settings,URLS

--- ATM/User/models.py
+++ ATM/User/models.py
@@ -1,30 +1,28 @@
 from django.db import models
 
-# Create your models here.
-
 class HolderType(models.Model):
     holderType_id = models.IntegerField(max_length=20)
     name = models.CharField(max_length=50)
-    #1st Holder 
-    #2nd Holder 
-    #Mover 
 
 class ATMUser(models.Model):
     id = models.BigAutoField(primary_key=True)
     username = models.CharField(max_length=255)
-    password = models.CharField(max_length=50)
-    IBAN = models.CharField(max_length=25)
-    BAN = models.CharField(max_length=21)
-    NIF = models.CharField(max_length=9)    
+    password = models.CharField(max_length=50)  # Campo para senha
+
+    def __str__(self):
+        return self.username
+
+class PIN(models.Model):
+    user = models.ForeignKey(ATMUser, on_delete=models.CASCADE)
+    pin = models.CharField(max_length=6)  # Campo para o PIN
 
 class BankAccount(models.Model):
     account_id = models.AutoField(primary_key=True)
-    holders = models.ManyToManyField(ATMUser,through='Holder')
-    balance = models.FloatField(decimal_place=2)
+    holders = models.ManyToManyField(ATMUser, through='Holder')
+    balance = models.FloatField(decimal_places=2)
     IBAN = models.CharField(max_length=25)
 
 class Holder(models.Model):
-    user = models.ForeignKey(ATMUser,on_delete=models.CASCADE)
-    account = models.ForeignKey(BankAccount,on_delete=models.CASCADE)
-    type = models.ForeignKey(HolderType,on_delete=models.CASCADE)
-
+    user = models.ForeignKey(ATMUser, on_delete=models.CASCADE)
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+    type = models.ForeignKey(HolderType, on_delete=models.CASCADE)
