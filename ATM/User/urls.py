d826612b83121984c3773956a74bd6ab324d057f
CarlosF80 <133024102+CarlosF80@users.noreply.github.com>
2023-11-14 15:42:57 +0000

    urls

--- ATM/User/urls.py
+++ ATM/User/urls.py
@@ -6,7 +6,7 @@ from .views import login_view, create_pin, change_pin
 from . import views
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('user/create_pin/', views.create_pin_view, name='create_pin'),
     path('user/change_pin/', views.change_pin_view, name='change_pin'),
 ]
